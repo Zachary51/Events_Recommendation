d6c201346cb671cd05696d1bf19969da07fff968
Zachary51 <zaricwong51@gmail.com>
2018-09-29 15:37:01 -0700

    recommendation

--- src/rpc/RpcHelper.java
+++ src/rpc/RpcHelper.java
@@ -1,8 +1,10 @@
 package rpc;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
@@ -30,5 +32,19 @@ public class RpcHelper {
 		response.addHeader("Access-Control-Allow-Origin", "*");
 		out.print(object);
 		out.close();
+	}
+	
+	public static JSONObject readJSONObject(HttpServletRequest request) {
+		StringBuilder sBuilder = new StringBuilder();
+		try (BufferedReader reader = request.getReader()) {
+			String line = null;
+			while((line = reader.readLine()) != null) {
+				sBuilder.append(line);
+			}
+			return new JSONObject(sBuilder.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new JSONObject();
 	}
 }
